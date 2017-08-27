9f7d88d923f706775e87bcab5da3baea1695ace1
Rainer Simon <rainer.simon@ait.ac.at>
2017-08-27 18:42:37 +0200

    Minimal data collection script

--- datacollector.rb
+++ datacollector.rb
@@ -1,4 +1,38 @@
+require 'active_record'
+
 require_relative 'models/ticker_data'
 require_relative 'services/kraken_client'
 
-puts 'Hello World'
+ActiveRecord::Base.establish_connection(
+  :adapter => "sqlite3",
+  :database => "datacollector.sqlite"
+)
+
+def store(ticker_response)
+  pairs = ticker_response['result'].keys
+
+  pairs.each do |pair|
+    data = ticker_response['result'][pair]
+
+    t = TickerData.new
+    t.trade_pair = pair
+    t.ask_price = data['a'][0].to_f
+    t.ask_volume = data['a'][2].to_f
+    t.bid_price = data['b'][0].to_f
+    t.bid_volume = data['b'][2].to_f
+    t.last_trade_price = data['c'][0].to_f
+    t.last_trade_volume = data['c'][1].to_f
+    t.save
+  end
+end
+
+kraken = Kraken::Client.new()
+while true do
+  begin
+    store(kraken.ticker('XBTEUR,BCHEUR'))
+  rescue
+    puts 'Error fetching current rates'
+  end
+
+  sleep 5 * 60
+end
--- schema_sqlite3.sql
+++ schema_sqlite3.sql
@@ -1,6 +1,7 @@
 CREATE TABLE ticker_data (
   id INTEGER PRIMARY KEY,
   created_at TEXT,
+  trade_pair TEXT,
   ask_price REAL,
   ask_volume REAL,
   bid_price REAL,
