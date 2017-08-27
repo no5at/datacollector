7e3370fd8349495078a35fa48ede3ae388cf1f29
Rainer Simon <rainer.simon@ait.ac.at>
2017-08-27 21:23:06 +0200

    Schema extension

---
+++ models/defcon.rb
@@ -0,0 +1,5 @@
+require 'active_record'
+
+class Defcon < ActiveRecord::Base
+  self.table_name = "ticker_data"
+end
---
+++ models/trigger.rb
@@ -0,0 +1,4 @@
+require 'active_record'
+
+class Trigger < ActiveRecord::Base
+end
