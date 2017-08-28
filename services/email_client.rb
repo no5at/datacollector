e2c42af157c429816866ee35b6f43456667dce2e
Rainer Simon <rainer.simon@ait.ac.at>
2017-08-28 19:04:12 +0200

    Dummy email service skeleton

--- services/email_client.rb
+++ services/email_client.rb
@@ -1,25 +1,26 @@
 require 'mail'
 
-#module MailService
+module MailService
+
   options = {
-    :address              => 'example.com',
+    :address              => 'smpt.example.com',
     :port                 => 25,
     :user_name            => 'user@example.com',
     :password             => 'password',
-    :authentication       => :login,
-    :tls                  => true
+    :authentication       => 'plain',
+    :enable_starttls_auto => true
   }
 
   Mail.defaults do
     delivery_method :smtp, options
   end
 
-  #def send(sendTo, sendFrom, subject, message)
+  def send(sendTo, sendFrom, subject, message)
     Mail.deliver do
        to to
        from from
        subject subject
-       body body
+       body message
     end
-  #end
-#end
+  end
+end
