bad360ba68b63e9b5085b0a3d5c0b33c94744f94
Rainer Simon <rainer.simon@ait.ac.at>
2017-09-03 18:24:55 +0200

    Minor edits

--- test/models/trigger_spec.rb
+++ test/models/trigger_spec.rb
@@ -10,10 +10,16 @@ class TriggerSpec < Test::Unit::TestCase
   def test_fired_triggers
     db = DB.new({ 'adapter' => 'sqlite3', 'database' => 'tmp/db_trigger_spec.sqlite' })
 
+    # Load fixtures, so that we have price history
+
+    # Add multiple triggers
+
+    # Check if the correct ones fire
+
     # trigger = Trigger.new
-    # trigger.trade_pair = B,
-    # trigger_type TEXT,
-    # threshold REAL
+    # trigger.trade_pair = "XBTEUR"
+    # trigger_type = "TRIPWIRE"
+    # threshold = 3800.0
 
     db.drop_database
   end
